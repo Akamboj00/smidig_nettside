30004df26248f6e75f62b98c038ee0f50054732a
Oscar Torgersen <56124359+TorgersenOscar@users.noreply.github.com>
2021-05-11 14:53:35 +0200

    Små-responsiv + mulig å rendre alt fra testdata

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import { BrowserRouter, Link } from "react-router-dom";
 import { Route, Switch } from "react-router";
 import { Header } from "../components/header/header";
+import { Learn } from '../components/learn/Learn'
 
 function Application() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path={"/learn"}>
-          <h1>This is the learn page</h1>
+          <Learn></Learn>
         </Route>
         <Route path={"/profile"}>
           <h1>This is the profile page</h1>
@@ -20,44 +20,11 @@ function Application() {
         </Route>
         <Route exact path={"/"}>
           <Header />
-        </Route>
+                  </Route>
         <Route>page not found</Route>
       </Switch>
     </BrowserRouter>
   );
-=======
-import {BrowserRouter, Link} from "react-router-dom";
-import {Route, Switch} from "react-router";
-import { Learn } from '../components/learn/Learn'
-
-function Application() {
-    return <BrowserRouter>
-        <Switch>
-            <Route path={"/learn"}>
-            <Learn/>
-            </Route>
-              <Route path={"/profile"}>
-                <h1>This is the profile page</h1>
-            </Route>
-              <Route path={"/report"}>
-                <h1>This is the report page</h1>
-            </Route>
-            <Route path={"/login"}>
-                <h1>This is the login page</h1>
-            </Route>
-            <Route exact path={"/"}>
-                <ul>
-                    <li><Link to={"/learn"}>Link to Learn</Link></li>
-                    <li><Link to={"/report"}>Link to report</Link></li>
-                    <li><Link to={"/profile"}>Link to Profile</Link></li>
-                </ul>
-            </Route>
-            <Route>
-                page not found
-            </Route>
-        </Switch>
-    </BrowserRouter>;
->>>>>>> cb17a77e937667f0383dfdf3c15d4a07e48a1043
 }
 
 ReactDOM.render(Application(), document.getElementById("app"));
--- src/components/learn/Learn.jsx
+++ src/components/learn/Learn.jsx
@@ -2,29 +2,57 @@ import React from "react"
 
 export function Learn(){
 
+
+    //bytter dette med request til firebase / eller backend
+    const testdata = [
+        {
+            product_id: 0,
+            product_name: "sunbelle",
+            product_img: "url:../img/test.png"
+        },
+        {
+            product_id: 1,
+            product_name: "sunbelle",
+            product_img: "url:../img/test.png"
+        },
+        {
+            product_id: 2,
+            product_name: "sunbelle",
+            product_img: "url:../img/test.png"
+        },
+        {
+            product_id: 3,
+            product_name: "sunbelle",
+            product_img: "url:../img/test.png"
+        },
+        {
+            product_id: 4,
+            product_name: "sunbelle",
+            product_img: "url:../img/test.png"
+        },
+        {
+            product_id: 5,
+            product_name: "sunbelle",
+            product_img: "url:../img/test.png"
+        }
+
+    ]
+
     return(
         <>
-        <div className="learn_container">
-            <div className="learn_heading">
+        <div className="learn_heading_container">
                 <h1 className="learn_text">Learn</h1>
                 <img className="learn_icon" src={require("url:../img/test.png")}></img>
                 <span className="line"></span>
             </div>
             <div className="learn_card_container">
-                <div className="learn_card">
-                    <img className="learn_card_icon" src={require("url:../img/sunbelle.png")}></img>
-                    <div class="learn_card_text">Learn Sunbell</div>
-                </div>
-                <div className="learn_card">
-                    <img className="learn_card_icon" src={require("url:../img/sunbelle.png")}></img>
-                    <div class="learn_card_text">Learn Sunbell</div>
-                </div>
-                <div className="learn_card">
-                    <img className="learn_card_icon" src={require("url:../img/sunbelle.png")}></img>
-                    <div class="learn_card_text">Learn Sunbell</div>
-                </div>
+            {testdata.map(({product_id, product_name, product_img}) => (
+                <div key={product_id} className="learn_card">
+                <img className="learn_card_icon" src={require(product_img)}></img>
+                <h2 className="learn_card_text">{product_name}</h2>
             </div>
-        </div>
+            ))}
+            </div>
         </>
     )
 }
