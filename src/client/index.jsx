a41ccacf6fc72704de93d834b8592c01d6f12636
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2021-05-11 13:26:30 +0200

    lagt til header på alle sider

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-import { BrowserRouter, Link } from "react-router-dom";
-import { Route, Switch } from "react-router";
-import { Header } from "../components/header/header";
-
-function Application() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path={"/learn"}>
-          <h1>This is the learn page</h1>
-        </Route>
-        <Route path={"/profile"}>
-          <h1>This is the profile page</h1>
-        </Route>
-        <Route path={"/report"}>
-          <h1>This is the report page</h1>
-        </Route>
-        <Route exact path={"/"}>
-          <Header />
-        </Route>
-        <Route>page not found</Route>
-      </Switch>
-    </BrowserRouter>
-  );
-=======
 import {BrowserRouter, Link} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import { Learn } from './Learn'
+import { Header } from '../components/header/header'
 
 function Application() {
     return <BrowserRouter>
+    <header>
+        <Header />
+    </header>
         <Switch>
             <Route path={"/learn"}>
             <Learn/>
@@ -46,18 +24,12 @@ function Application() {
                 <h1>This is the login page</h1>
             </Route>
             <Route exact path={"/"}>
-                <ul>
-                    <li><Link to={"/learn"}>Link to Learn</Link></li>
-                    <li><Link to={"/report"}>Link to report</Link></li>
-                    <li><Link to={"/profile"}>Link to Profile</Link></li>
-                </ul>
             </Route>
             <Route>
                 page not found
             </Route>
         </Switch>
     </BrowserRouter>;
->>>>>>> cb17a77e937667f0383dfdf3c15d4a07e48a1043
 }
 
 ReactDOM.render(Application(), document.getElementById("app"));
