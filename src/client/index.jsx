3de99145636797f9855a1c5d49a0082d53f14beb
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2021-05-11 13:15:38 +0200

    fått lagt til header og linking fungerer, mangler finpussing på stylingen

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link} from "react-router-dom";
-import {Route, Switch} from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import { Header } from "../components/header/header";
 
 function Application() {
-    return <BrowserRouter>
-        <Switch>
-            <Route path={"/learn"}>
-                <h1>This is the learn page</h1>
-            </Route>
-              <Route path={"/profile"}>
-                <h1>This is the profile page</h1>
-            </Route>
-              <Route path={"/report"}>
-                <h1>This is the report page</h1>
-            </Route>
-            <Route exact path={"/"}>
-                <ul>
-                    <li><Link to={"/learn"}>Link to Learn</Link></li>
-                    <li><Link to={"/report"}>Link to report</Link></li>
-                    <li><Link to={"/profile"}>Link to Profile</Link></li>
-                </ul>
-            </Route>
-            <Route>
-                page not found
-            </Route>
-        </Switch>
-    </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={"/learn"}>
+          <h1>This is the learn page</h1>
+        </Route>
+        <Route path={"/profile"}>
+          <h1>This is the profile page</h1>
+        </Route>
+        <Route path={"/report"}>
+          <h1>This is the report page</h1>
+        </Route>
+        <Route exact path={"/"}>
+          <Header />
+        </Route>
+        <Route>page not found</Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(Application(), document.getElementById("app"));
