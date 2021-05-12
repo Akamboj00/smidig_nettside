ad0205adcb054fe62276d94e64df067ecc68a3ab
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2021-05-12 12:06:09 +0200

    startet på main menu siden, skal bruke grid her

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,35 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link} from "react-router-dom";
-import {Route, Switch} from "react-router";
-import { Learn } from '../components/learn/learn'
-import { Header } from '../components/header/header'
+import { BrowserRouter, Link } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import { Learn } from "../components/learn/learn";
+import { Header } from "../components/header/header";
+import { MainMenu } from "../client/mainMenu/mainMenu";
 
 function Application() {
-    return <BrowserRouter>
-    <header>
+  return (
+    <BrowserRouter>
+      <header>
         <Header />
-    </header>
-        <Switch>
-            <Route path={"/learn"}>
-            <Learn/>
-            </Route>
-              <Route path={"/profile"}>
-                <h1>This is the profile page</h1>
-            </Route>
-              <Route path={"/report"}>
-                <h1>This is the report page</h1>
-            </Route>
-            <Route path={"/login"}>
-                <h1>This is the login page</h1>
-            </Route>
-            <Route exact path={"/"}>
-            </Route>
-            <Route>
-                page not found
-            </Route>
-        </Switch>
-    </BrowserRouter>;
+      </header>
+      <Switch>
+        <Route path={"/learn"}>
+          <Learn />
+        </Route>
+        <Route path={"/profile"}>
+          <h1>This is the profile page</h1>
+        </Route>
+        <Route path={"/report"}>
+          <h1>This is the report page</h1>
+        </Route>
+        <Route path={"/login"}>
+          <h1>This is the login page</h1>
+        </Route>
+        <Route exact path={"/"}>
+          <MainMenu />
+        </Route>
+        <Route>page not found</Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(Application(), document.getElementById("app"));
