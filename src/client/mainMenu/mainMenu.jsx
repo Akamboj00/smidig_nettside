69aa3cef84d8e1035d58dca2de0f3aa756d85def
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2021-05-19 14:31:23 +0200

    fått inn grid, manlger bare å lage de onclick

--- src/client/mainMenu/mainMenu.jsx
+++ src/client/mainMenu/mainMenu.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./mainMenu.css";
+import Link from "react-router-dom";
 
 export const MainMenu = () => {
   return (
     <div className="main_container">
-      <div className="box1">
+      <div className="box" id="box1">
         <h1>Learn</h1>
       </div>
-      <div className="box2">
+
+      <div className="box" id="box2">
         <h1>Report</h1>
       </div>
-      <div className="box3">
+
+      <div className="box" id="box3">
         <h1>Profile</h1>
       </div>
     </div>
