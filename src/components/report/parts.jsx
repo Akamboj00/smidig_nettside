953b975952f2d86503704c10fa1e7e27f772270a
Oscar Torgersen <56124359+TorgersenOscar@users.noreply.github.com>
2021-05-28 22:53:31 +0200

    :/

--- src/components/report/parts.jsx
+++ src/components/report/parts.jsx
@@ -48,12 +48,15 @@ export function Parts() {
                     <div className="parts_card_container">
                         {products.map(({part_id, part_image, part_name}) => (
                                 <Link
+                                style={{textDecoration: "none"}}
                                 key={part_id}
-                                className="learn_card"
+                                className="parts_report_card"
                                 to={{ pathname: `/report/${sessionStorage.getItem("current_report_product_name").toLowerCase()}/${part_id}`, part: part_id, _part_name: part_name}}
                                 >
-                                <img className="learn_card_icon" src={part_image} />
-                                <div className="card_info">
+                                <div className="parts_image_container">
+                                    <img className="parts_image" src={part_image} />
+                                </div>
+                                <div className="parts_card_info">
                                     <h4 className="learn_card_text">{part_name}</h4>
                                 </div>
                                 </Link>
