da3983177a3eee2427f6f55c71ef6d4617003a7a
Oscar Torgersen <56124359+TorgersenOscar@users.noreply.github.com>
2021-05-31 13:27:46 +0200

    Report

--- src/components/report/fileReport.jsx
+++ src/components/report/fileReport.jsx
@@ -2,17 +2,38 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import fire from "../../server/firebase";
 import { LoadingView } from "../loadingView";
-import {Form, Button} from 'react-bootstrap'
-import { errorView } from '../errorView'
-import Alert from 'react-bootstrap/Alert'
+import {Form, Button} from 'react-bootstrap';
+import { errorView } from '../errorView';
+import Alert from 'react-bootstrap/Alert';
+import {useHistory} from "react-router";
 
 export function FileReport() {
     const [error, setError] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
+    const history = useHistory();
+    let location = useLocation()
+    if(!location.part && !location._part_name){
+        history.push("/report")
+    }
     const submitReport = () => {
+        console.log(location.part)
 
-        const part_number = document.getElementsByClassName("file_report_input").value
+        var part_number = document.getElementById("part_number").value
+        var optional_comment = document.getElementById("optional_comment").value
+        console.log(part_number)
         if(!part_number){
-            setError(true)
+            setError("You need to fill in part number!")
+        }else{
+            setError(false)
+            fire.database().ref('reports').push({
+                    userId : sessionStorage.getItem('user'),
+                    partNumber : part_number,
+                    productId : location.part,
+                    partName: location._part_name,
+                    productName : sessionStorage.getItem("current_report_product_name"),
+                    comment : optional_comment
+            })
+            setSubmitted(true)
         }
     }
     return(
@@ -26,14 +47,17 @@ export function FileReport() {
                         <div className={"container_inner"}>
                         <Form.Group className="file_report_form">
                             <h3>Part number*</h3>
-                            <Form.Control className="file_report_input" size="lg" type="text" placeholder="part number*" />
+                            <Form.Control id="part_number" className="file_report_input" size="lg" type="text" placeholder="part number*" />
                             <br />
                             <h3>Comment (optional)</h3>
-                            <Form.Control className="file_report_input" size="lg" type="text" placeholder="optional comment" />
+                            <Form.Control id="optional_comment" className="file_report_input_comment" size="lg" type="text" placeholder="optional comment" />
                             <br/>
                             <Button type="submit" onClick={() => submitReport()} >Submit report</Button>
                             {error && <Alert className="error" variant="danger">
-                                <h5>You need to fill in part number!</h5>
+                                <h5>{error}</h5>
+                            </Alert>}
+                            {submitted && <Alert className="error" variant="success">
+                                <h5>Report submitted!</h5>
                             </Alert>}
                         </Form.Group>
                         </div>
