3e1a13b47c159c1c5141ea4fa6d1ed9a30b6929e
mackemilian <maximi0202@gmail.com>
2021-06-01 13:16:30 +0200

    .

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./product.css";
-import { Link, useLocation } from "react-router-dom";
-import { useParams } from 'react-router-dom';
+import {Link, useLocation} from "react-router-dom";
+import {useParams} from 'react-router-dom';
 import fire from "../../server/firebase";
-import { getDatabaseSingleProgress } from '../../components/lib/fb'
-import { LoadingView } from "../loadingView";
+import {getDatabaseSingleProgress} from '../../components/lib/fb'
+import {LoadingView} from "../loadingView";
+import {CheckmarkEmptyIcon, CheckmarkIcon} from "../svg/svg";
 
 require("url:../img/battery.png");
 require("url:../img/lampshade.png");
@@ -12,78 +13,102 @@ require("url:../img/solarpanel.png");
 require("url:../img/turtlelamp.png");
 
 export const Product = () => {
-  const [products, setProducts] = useState(null);
-  const [productsOnUsers, setProductsOnUsers] = useState(null)
-  const [authKey, setAuthKey] = useState(() => {
-    let key = Object.keys(window.sessionStorage)
-        .filter(item => item.startsWith('firebase:authUser'))[0];
-    if (key === undefined) return history.push("/login");
-    return key;
-});
-  let location = useLocation();
-  const { id } = useParams();
-
-async function getDatabaseSingleProgress() {
-  const dbReference = fire.database().ref();
-
-    dbReference.child("progress").child(sessionStorage.getItem("current_product_id")).once('value').then((snapshot) => {
-        setProducts(snapshot.val());
+    const [products, setProducts] = useState(null);
+    const [productsOnUsers, setProductsOnUsers] = useState(null)
+    const [authKey, setAuthKey] = useState(() => {
+        let key = Object.keys(window.sessionStorage)
+            .filter(item => item.startsWith('firebase:authUser'))[0];
+        if (key === undefined) return history.push("/login");
+        return key;
     });
-    const authUser = JSON.parse(sessionStorage.getItem(authKey.toString()));
+    let location = useLocation();
+    const {id} = useParams();
 
-    dbReference.child('users').child(authUser.uid).once('value').then((snapshot) => {
-      setProductsOnUsers(snapshot.val())
-    })
-}
-useEffect(() => {
-  getDatabaseSingleProgress()
-}, []);
+    async function getDatabaseSingleProgress() {
+        const dbReference = fire.database().ref();
 
-if(location._product_name && location._product_id){
-  sessionStorage.setItem("current_product", location._product_name)
-  sessionStorage.setItem("current_product_id", location._product_id)
-  }
-if(authKey === null){
-  history.push("/users")
-}
-if(products === null || productsOnUsers === null){
-    return(
-      <div id={"container_main"}>
-        <div className="learn_card_container">
-          <LoadingView></LoadingView>
-        </div>
-      </div>
-    )
-}else{
-  return (
-    <>
-      <div id={"container_main"}>
-        <h2 className="product_name">{sessionStorage.getItem("current_product")}</h2>
-        <div className={"wrap_container wrap_content"}>
-                    <div className={"container_inner"}>
-        <div className="learn_card_container">
-          {products.map(({part_id, part_image, part_name, part_video}) => (
-            <Link
-              style={
-                { textDecoration: "none",
-                  backgroundColor: (productsOnUsers[sessionStorage.getItem("user")].progress[id][part_id]) ? "green" : "red"
-              }
-            }
-              key={part_id}
-              className="learn_card"
-              to={{ pathname: "/video", video: part_video, product: id, part: part_id, _part_name: part_name}}
-            >
-              <img className="learn_card_icon" src={part_image} />
-              <div className="card_info">
-                <h4 className="learn_card_text">{part_name}</h4>
-              </div>
-            </Link>
-          ))}
-          </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+        dbReference.child("progress").child(sessionStorage.getItem("current_product_id")).once('value').then((snapshot) => {
+            setProducts(snapshot.val());
+        });
+        const authUser = JSON.parse(sessionStorage.getItem(authKey.toString()));
+
+        dbReference.child('users').child(authUser.uid).once('value').then((snapshot) => {
+            setProductsOnUsers(snapshot.val())
+        })
+    }
+
+    useEffect(() => {
+        getDatabaseSingleProgress()
+    }, []);
+
+    if (location._product_name && location._product_id)
+    {
+        sessionStorage.setItem("current_product", location._product_name)
+        sessionStorage.setItem("current_product_id", location._product_id)
+    }
+    if (authKey === null)
+    {
+        history.push("/users")
+    }
+    if (products === null || productsOnUsers === null)
+    {
+        return (
+            <div id={"container_main"}>
+                <div className="learn_card_container">
+                    <LoadingView></LoadingView>
+                </div>
+            </div>
+        )
+    }
+    else
+    {
+        return (
+            <>
+                <div id={"container_main"}>
+                    <h2 className="product_name">{sessionStorage.getItem("current_product")}</h2>
+                    <div className={"wrap_container wrap_content"}>
+                        <div className={"container_inner"}>
+                            <div className="learn_card_product_container">
+                                {products.map(({part_id, part_image, part_name, part_video}) => (
+                                    <Link
+                                        style={
+                                            {
+                                                textDecoration: "none",
+                                                backgroundColor: (productsOnUsers[sessionStorage.getItem("user")].progress[id][part_id]) ? "#f2f2f2" : "#ffffff",
+                                            }
+                                        }
+                                        key={part_id}
+                                        className="learn_card_product"
+                                        to={{
+                                            pathname: "/video", video: part_video, product: id, part: part_id,
+                                            _part_name: part_name
+                                        }}
+                                    >
+                                        <div className={"learn_card_product_watched_container"}>
+                                            <div className={"learn_card_product_watched center"}>
+                                                {(productsOnUsers[sessionStorage.getItem("user")].progress[id][part_id]) ? (<CheckmarkIcon
+                                                    className={"checkmark_icon_learn_product center"}
+                                                />) : (  <CheckmarkEmptyIcon
+                                                    className={"checkmark_icon_learn_product_empty center"}
+                                                />)}
+                                            </div>
+                                        </div>
+                                        <div className={"learn_product_image_container"}>
+                                            <img className="learn_card_icon_product center" src={part_image}/>
+                                        </div>
+                                        <div className="card_info_product center">
+                                            <div className="tooltip_learn">
+                                                <span className="tooltiptext"><p className={"center"}>{part_name}</p></span>
+                                            </div>
+                                            <h6 className="learn_card_text_product">{part_name}</h6>
+                                        </div>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -1,62 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import VideoPlayer from "react-video-js-player";
-import { Link, useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import fire from "../../server/firebase";
 import {useHistory} from "react-router";
 import "./video.css";
 
 export const Video = () => {
-  const [authKey, setAuthKey] = useState(() => {
-  let key = Object.keys(window.sessionStorage)
-  .filter(item => item.startsWith('firebase:authUser'))[0];
-    if (key === undefined) return history.push("/login");
-    return key;
-  });
-  let location = useLocation();
-  const history = useHistory();
+    const [authKey, setAuthKey] = useState(() => {
+        let key = Object.keys(window.sessionStorage)
+            .filter(item => item.startsWith('firebase:authUser'))[0];
+        if (key === undefined) return history.push("/login");
+        return key;
+    });
+    let location = useLocation();
+    const history = useHistory();
 
 
-    if(location.video === undefined){
-      history.push(`${"/product/"+sessionStorage.getItem("current_product_id")}`);
+    if (location.video === undefined)
+    {
+        history.push(`${"/product/" + sessionStorage.getItem("current_product_id")}`);
     }
 
-    const videoDone = () => {   
+    const videoDone = () => {
 
-      const authUser = JSON.parse(sessionStorage.getItem(authKey.toString()));
+        const authUser = JSON.parse(sessionStorage.getItem(authKey.toString()));
 
-    fire.database()
-    .ref('users')
-    .child(authUser.uid)
-    .child(sessionStorage.getItem("user"))
-    .child("progress")
-    .child(location.product)
-    .child(location.part)
-    .set(fire.database.ServerValue = 1)
+        fire.database()
+            .ref('users')
+            .child(authUser.uid)
+            .child(sessionStorage.getItem("user"))
+            .child("progress")
+            .child(location.product)
+            .child(location.part)
+            .set(fire.database.ServerValue = 1)
 
-        history.push(`${"/product/"+sessionStorage.getItem("current_product_id")}`);
+        history.push(`${"/product/" + sessionStorage.getItem("current_product_id")}`);
     }
-    if(location.product){
-      sessionStorage.setItem("current_product_id", location.product)
+    if (location.product)
+    {
+        sessionStorage.setItem("current_product_id", location.product)
     }
 
-  return (
-    <section>
-      <div className="videoContainer">
-        <div className="player">
-          <div className="headline">
-            <h4>{location._part_name}</h4>
-          </div>
-          <VideoPlayer
-            onEnd={()=> {videoDone()}}
-            src={location.video}
-            type="video/mp4"
-            width="750"
-            playbackRates={false}
-          />
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <div className="videoContainer">
+                <div className="player">
+                    <div className="headline">
+                        <h5>{location._part_name}</h5>
+                    </div>
+                    <VideoPlayer
+                        onEnd={() => {videoDone()}}
+                        src={location.video}
+                        type="video/mp4"
+                        width="750"
+                        playbackRates={false}
+                    />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Video;
